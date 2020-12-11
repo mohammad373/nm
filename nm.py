2fa6096090bf51ae5449eabde0e4e64afc730de5
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-11 17:00:52 +0330

    Update nm.py

--- nm.py
+++ nm.py
@@ -10,13 +10,13 @@ from colorama import Fore
 
 my_list = ["cpanel" , "bot.txt"  , "admin"] 
 
-def __web__():
-    os.system("clear")
-    time.sleep(1)
-    print(Fore.YELLOW + "Hello . Welcome Back ;)")
-    time.sleep(1)
-    target = input(Fore.BLUE + "\n[" + Fore.RED + "*" + Fore.BLUE + "]" + Fore.RED + " ~ " + Fore.YELLOW + "Pleass Enter The address Target " + Fore.GREEN + "==>  ")
-    if target == "" or None:
+
+os.system("clear")
+time.sleep(1)
+print(Fore.YELLOW + "Hello . Welcome Back ;)")
+time.sleep(1)
+target = input(Fore.BLUE + "\n[" + Fore.RED + "*" + Fore.BLUE + "]" + Fore.RED + " ~ " + Fore.YELLOW + "Pleass Enter The address Target " + Fore.GREEN + "==>  ")
+if target == "" or None:
         try:
             time.sleep(1)
             print(Fore.RED + "Error : Your Target Is Not Found Or None ;(")
@@ -24,24 +24,24 @@ def __web__():
             sys.exit()
         except:
             pass
-    if not "https" in target or not "http" in target:
+if not "https" in target or not "http" in target:
         target = "http://" + target
-    else:
+else:
         pass
     
-    s = socket.gethostbyname(target)
-    time.sleep(1)
-    print(Fore.GREEN + "[+] ~ Your Ip Target is  :  " + Fore.BLUE + s)
-    time.sleep(1)
-    r = requests.get(target + "/" + "/wp-content/plugins/")
-    if r.status_code == 404 or r.status_code == 500:
+s = socket.gethostbyname(target)
+time.sleep(1)
+print(Fore.GREEN + "[+] ~ Your Ip Target is  :  " + Fore.BLUE + s)
+time.sleep(1)
+r = requests.get(target + "/" + "/wp-content/plugins/")
+if r.status_code == 404 or r.status_code == 500:
         try:
             print(Fore.RED + "[!] ~ Your Target Is Not WordPress ;(")
             time.sleep(1)
             sys.exit()
         except:
             pass
-    else:
+else:
 
             print(Fore.GREEN + "[*] ~ Your Target Is WordPress ;)")
             time.sleep(1)
@@ -54,4 +54,4 @@ def __web__():
                         print(Fore.GREEN + w + Fore.YELLOW + " > " + Fore.BLUE + "Found ;)")
                     
 
-__web__()
+
